a263c23152d7fdb89210c00269c2f9ba27e67056
Izabella German <113433601+IzabellaGerman@users.noreply.github.com>
2024-04-15 16:35:00 +0200

    adding numbers to textbox with pressing buttons

--- OrdinaryCalc/MainWindow.xaml.cs
+++ OrdinaryCalc/MainWindow.xaml.cs
@@ -24,5 +24,68 @@ namespace OrdinaryCalc
         {
             InitializeComponent();
         }
-    }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+            {
+
+            }
+
+        private void Button_ClickClear(object sender, RoutedEventArgs e)
+            {
+            CountTextBox.Text = "0";
+            }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+            {
+
+            CountTextBox.Text = CountTextBox.Text == "0"?(CountTextBox.Text = "1"):(CountTextBox.Text +=  "1");
+
+            }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+            {
+            CountTextBox.Text = CountTextBox.Text == "0" ? (CountTextBox.Text = "2") : (CountTextBox.Text += "2");
+            }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+            {
+            CountTextBox.Text = CountTextBox.Text == "0" ? (CountTextBox.Text = "3") : (CountTextBox.Text += "3");
+            }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+            {
+            CountTextBox.Text = CountTextBox.Text == "0" ? (CountTextBox.Text = "4") : (CountTextBox.Text += "4");
+            }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+            {
+            CountTextBox.Text = CountTextBox.Text == "0" ? (CountTextBox.Text = "5") : (CountTextBox.Text += "5");
+            }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+            {
+            CountTextBox.Text = CountTextBox.Text == "0" ? (CountTextBox.Text = "6") : (CountTextBox.Text += "6");
+            }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+            {
+            CountTextBox.Text = CountTextBox.Text == "0" ? (CountTextBox.Text = "7") : (CountTextBox.Text += "7");
+            }
+      
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+            {
+            CountTextBox.Text = CountTextBox.Text == "0" ? (CountTextBox.Text = "8") : (CountTextBox.Text += "8");
+            }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+            {
+            CountTextBox.Text = CountTextBox.Text == "0" ? (CountTextBox.Text = "9") : (CountTextBox.Text += "9");
+            }
+
+        private void Button_Click_0(object sender, RoutedEventArgs e)
+            {
+            CountTextBox.Text = CountTextBox.Text == "0" ? (CountTextBox.Text = "0") : (CountTextBox.Text += "0");
+            }
+        }
 }
